59ac108464bf65fc34461d2ffd07cd0a0275736b
FilipeMartins <filipe@bling.eu>
2022-09-07 08:59:40 +0200

    feat: input via terminal

--- main.py
+++ main.py
@@ -2,4 +2,14 @@ from mars.mars_robot import Robot
 
 
 if __name__ == "__main__":
-    test = Robot().is_robot_bounded("GLRLLGLL")
+    top_right = input("Enter plateau top-right: ")
+    current_pos = input("Enter current position and orientation: ")
+    orders = input("Enter robot orders: ")
+    robot = Robot()
+    print(robot.compute_orders(
+        {
+            "map_top_right": top_right,
+            "current_pos": current_pos,
+            "orders": orders
+        }
+    ))
